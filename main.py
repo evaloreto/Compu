cbf3a480186be7aca3fa7c26693f82617a0cc9ba
Jose <eva.tudanca92@gmail.com>
2017-11-23 22:51:49 +0100

    Se corrigen errores en los html. Se añade una query a la bbdd dependiendo del umbral si se proporciona uno. Se corrige el guardar en la bbdd el valor como tipo float, estaba como string y no se podia hacer una query comparando valores

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import re
 import time
 import requests
@@ -12,7 +12,7 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/result', methods=['GET'])
+@app.route('/result', methods=['GET', 'POST'])
 def result():
     web_num_aleatorio = requests.get('http://www.numeroalazar.com.ar/')
     if web_num_aleatorio.status_code == 200:
@@ -20,7 +20,7 @@ def result():
         m = r.search(web_num_aleatorio.text)
         if m:
             num_busqueda = re.search("\d+(?:\.\d+)?", m.group(1))
-            num = num_busqueda.group()
+            num = float(num_busqueda.group())
             hora_busqueda = time.strftime("%H:%M:%S")
             fecha_busqueda = time.strftime("%x")
 
@@ -38,11 +38,21 @@ def result():
                 }
             )
 
-            query = db.aleatorios.find()
+            if request.method == "GET":
+                query = db.aleatorios.find()
+            elif request.method == "POST":
+                valormedio = request.form.get('valormedio', default=False, type=bool)
+                umbral = request.form.get('umbral', default=-1, type=int)
+                if umbral == -1:
+                    query = db.aleatorios.find()
+                else:
+                    query = db.aleatorios.find({'valor': {'$gt': umbral}})
+                    #for document in query:
+                    #    print document['valor']
+
             #for document in query:
             #    print document['valor']
             #    print document['hora']
             #    print document['fecha']
 
-            return render_template('results.html', query=query)
-
+        return render_template('results.html', query=query)
